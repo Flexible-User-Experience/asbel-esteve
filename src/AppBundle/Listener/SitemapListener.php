716ef949ebe6dcf7e544d5d649939e9d9be642a3
David Romaní <david@flux.cat>
2016-01-13 10:22:34 +0100

    improve sitemap 3

--- src/AppBundle/Listener/SitemapListener.php
+++ src/AppBundle/Listener/SitemapListener.php
@@ -2,12 +2,17 @@
 
 namespace AppBundle\Listener;
 
+use AppBundle\Entity\Film;
 use AppBundle\Menu\FrontendMenuBuilder;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\EntityManager;
 use Knp\Menu\MenuItem;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Presta\SitemapBundle\Service\SitemapListenerInterface;
 use Presta\SitemapBundle\Event\SitemapPopulateEvent;
 use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Routing\RouterInterface;
 
 /**
  * Class SitemapListener
@@ -18,6 +23,12 @@ use Presta\SitemapBundle\Sitemap\Url\UrlConcrete;
  */
 class SitemapListener implements SitemapListenerInterface
 {
+    /** @var RouterInterface */
+    private $router;
+
+    /** @var EntityManager */
+    private $em;
+
     /** @var RequestStack */
     private $rs;
 
@@ -27,16 +38,24 @@ class SitemapListener implements SitemapListenerInterface
     /** @var string */
     private $baseRoute;
 
+    /** @var ArrayCollection  */
+    private $contentPages;
+
     /**
      * SitemapListener constructor
      *
+     * @param RouterInterface     $router
+     * @param EntityManager       $em
      * @param RequestStack        $rs
      * @param FrontendMenuBuilder $fmb
      */
-    public function __construct(RequestStack $rs, FrontendMenuBuilder $fmb)
+    public function __construct(RouterInterface $router, EntityManager $em, RequestStack $rs, FrontendMenuBuilder $fmb)
     {
+        $this->router = $router;
+        $this->em = $em;
         $this->rs = $rs;
         $this->fmb = $fmb;
+        $this->contentPages = $this->em->getRepository('AppBundle:Film')->findAllEnabledSortedByPublishDateDescWithJoin();
 //        $this->baseRoute = $this->rs->getCurrentRequest()->getSchemeAndHttpHost();
         $this->baseRoute = 'http://www.asbelesteve.com'; // TODO decouple this hardcoded route
     }
@@ -56,6 +75,23 @@ class SitemapListener implements SitemapListenerInterface
                     ->addUrl(
                         new UrlConcrete(
                             $this->baseRoute . $child->getUri(),
+                            new \DateTime(),
+                            UrlConcrete::CHANGEFREQ_HOURLY,
+                            1
+                        ),
+                        'default'
+                    );
+            }
+            /** @var Film $contentPage */
+            foreach ($this->contentPages as $contentPage) {
+                $url = $this->router->generate('app_content', array(
+                    'slug' => $contentPage->getSlug(),
+                ), UrlGeneratorInterface::ABSOLUTE_URL);
+                $event
+                    ->getGenerator()
+                    ->addUrl(
+                        new UrlConcrete(
+                            $url,
                             new \DateTime(),
                             UrlConcrete::CHANGEFREQ_HOURLY,
                             1
